f5ae71af2a6993453bfacd9dcbbb56ac1c80fc78
gokul <gokulmittu01@gmail.com>
2026-07-30 20:30:52 +0530

    uPdated lenis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import Lenis from 'lenis';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,40 @@ import { FooterComponent } from './footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'burgers';
+  private lenis!: Lenis;
+  private reqId: number | null = null;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.lenis = new Lenis({
+        duration: 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // standard easing
+        smoothWheel: true,
+        wheelMultiplier: 1, // acts like smooth: 1
+        touchMultiplier: 0.1, // acts like smoothTouch: 0.1
+      });
+
+      const raf = (time: number) => {
+        this.lenis.raf(time);
+        this.reqId = requestAnimationFrame(raf);
+      };
+
+      this.reqId = requestAnimationFrame(raf);
+    }
+  }
+
+  ngOnDestroy() {
+    if (isPlatformBrowser(this.platformId)) {
+      if (this.reqId !== null) {
+        cancelAnimationFrame(this.reqId);
+      }
+      if (this.lenis) {
+        this.lenis.destroy();
+      }
+    }
+  }
 }
